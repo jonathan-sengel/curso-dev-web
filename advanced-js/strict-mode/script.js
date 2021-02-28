356df7fed8a116b13c9270e305250a6485da570e
Jonathan Engel <joow.engeel@gmail.com>
2021-02-28 13:52:30 -0300

    exemplo de strict mode

---
+++ advanced-js/strict-mode/script.js
@@ -0,0 +1,5 @@
+"use strict"; //define que seja obrigatorio a declaração de variavel antes de usa-la. - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Strict_mode
+
+// a = 1; // não permite "auto declarar" uma variável
+
+let a = 1;
