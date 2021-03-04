2a1b6e5efa744f0c3b749ac008f2f549c80f07e1
Jonathan Engel <joow.engeel@gmail.com>
2021-03-03 21:43:42 -0300

    efeito para o score do jogador atual ajustado

--- jogo-da-velha/scripts/game.js
+++ jogo-da-velha/scripts/game.js
@@ -2,6 +2,7 @@
 
 let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
+let lastPlayer=0;
 let symbols = ['o', 'x'];
 let gameOver = false;
 let boardPosition;
@@ -18,17 +19,22 @@ let winStates = [
 
 function handleMove(position) {
 
+    //se gameOver for igual a true ele entra neste if e retorna true;
     if (gameOver) {
         return;
     }
 
-
+    //valida se o board no index referente ao position (que é o id do square clicado) está vazio, só entra no if se estiver
     if (board[position] == '') {
+        
+        // define no index refere ao position dentro do array board o simbolo do player 1 ou 2, por padrão a primeira chamada será player 1 "O";
         board[position] = symbols[playerTime];
 
+        //atribui a variavel gameOver a function isWin (ela valida se a ultima jogada foi vencedora);
         gameOver = isWin();
         if (!gameOver) {
 
+            lastPlayer= playerTime;
             // ternario, se playertime for igual a zero ? (siginifica que a variavel playerTime será igual ao que vem após o interrogação, senão receba o valor após o :
             playerTime = (playerTime == 0) ? 1 : 0;
 
@@ -44,18 +50,24 @@ function handleMove(position) {
     return gameOver;
 }
 
+
+//function que verifica se a ultima jogada foi vencedora;
 function isWin() {
 
+    //laço de repetição dentro do winStates (que é o array que define os estados de vitória). Então para cada index dentro deste array ele retorna o state;
     for (state of winStates) {
 
+        //atribui estas 3 variaves passando a informação de cada item dentro do array winStates;
         let pos1 = state[0];
         let pos2 = state[1];
         let pos3 = state[2];
 
+        //caso essa validação abaixo seja verdadeira, significa que existe uma das posições de vitoria definida no array winStates, então ele retorna true para o isWin();
         if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != '') {
             return true;
         }
     }
+    //caso contrário retorna false;
     return false;
 
 }
--- jogo-da-velha/scripts/interface.js
+++ jogo-da-velha/scripts/interface.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     squares.forEach(square => {
         square.addEventListener('click', handleClick);
     });
+    currentPlayer();
 });
 
 //função executada ao clicar em um elemento .square;
@@ -16,7 +17,6 @@ function handleClick(event) {
     let square = event.target;
     //captura o ID do elemento .square clicado e joga na variavel position;
     let position = square.id;
-    console.log(square.id);
 
     // se o retorno da function handleMove passando o argumento position retornar true, ele entra no if;
     if (handleMove(position)) {
@@ -25,20 +25,45 @@ function handleClick(event) {
             alert('O Jogo acabou!');
         }, 50);
     }
+
+
     //chama a function para atualização da visualização do .square clicado exibindo a imagem definida ao usuário (x/o)
     updateSquare(position);
+
 }
 
-//function que atualiza a view do campo clicado na pagina;
+//function que atualiza a view do campo clicado na pagina recebendo como argumento um position;
 function updateSquare(position) {
+    //se o position vir como vazio, ele grava no console log a mensagem abaixo, nao permitindo estourar erro e nem modificando nada;
     if (position == '') {
         console.log('> Por favor clique em um quadrado vazio!');
     }
     else {
+
+        //joga na variavel square o id do .square clicado que foi passado na chamada da função "updateSquare";
         let square = document.getElementById(position.toString());
+
+        //declara variavel symbol recebendo o index referente ao ID do square clicado , sendo assim ele vai definir em symbol se o clique foi do usuário O ou X;
         let symbol = board[position];
+
+        // o square clicado pelo usuário receberá uma div com a class "o" ou "x", o que fará atualizar a visualização de acordo com o ::after configurado no CSS;
         square.innerHTML = `<div class="${symbol}"> </div>`;
+        currentPlayer(playerTime);
     }
+}
+
+function currentPlayer() {
+    let scoreElementLast = document.getElementById(`p${lastPlayer}`);
+    let scoreElementNext = document.getElementById(`p${playerTime}`);
+    
+    scoreElementLast.classList.remove('selected');
+    scoreElementNext.classList.add('selected');
+
+
+
+
+
+    
 }
 
 // esta função atualizava todos os quadrados da tela, então foi a função acima que atualiza apenas o quadrado clicado;
