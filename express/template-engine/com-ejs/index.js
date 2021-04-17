df5381accfb23e074bea7a36eee2e0d14bd8207e
Jonathan Engel <joow.engeel@gmail.com>
2021-04-17 17:57:08 -0300

    template engine com ejs final

--- express/template-engine/com-ejs/index.js
+++ express/template-engine/com-ejs/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
+let users = [
+    {
+        id: 0,
+        name: 'James Cameron',
+        phone: '(87)9955-4455'
+    },
+    {
+        id: 1,
+        name: 'Romário',
+        phone: '(87)77554-4455'
+    },
+    {
+        id: 2,
+        name: 'Joaquina',
+        phone: '(54)6655-4455'
+    },
+];
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('user', { users: users });
+});
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => console.log('Server listening on 3000'));
